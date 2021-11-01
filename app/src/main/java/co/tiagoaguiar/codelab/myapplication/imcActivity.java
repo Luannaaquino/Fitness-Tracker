09cc5b5bf96af9efc4834341a440046987be2327
Luanna Aquino <luannaluleticia@gmail.com>
2021-11-01 15:13:44 -0300

    Initial commit

---
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/imcActivity.java
@@ -0,0 +1,15 @@
+package co.tiagoaguiar.codelab.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class imcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
