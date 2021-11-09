e60b8f0c4ea3f7ac27f6aaa6417cd2e1c2d71512
Luanna Aquino <luannaluleticia@gmail.com>
2021-11-09 17:11:42 -0300

    Coleções de Views Recicláveis

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -3,25 +3,61 @@ package co.tiagoaguiar.codelab.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-	private View btnImc;
+	// private View btnImc;
+		private RecyclerView rvMain;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		btnImc = findViewById(R.id.button_imc);
-		btnImc.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-				startActivity(intent);
-			}
-		});
+	//	btnImc = findViewById(R.id.button_imc);
+	//	btnImc.setOnClickListener(new View.OnClickListener() {
+	//		@Override
+	//		public void onClick(View view) {
+	//			Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+	//			startActivity(intent);
+	//		}
+	//	});
+
+		rvMain = findViewById(R.id.main_rv);
+		rvMain.setLayoutManager(new LinearLayoutManager(this));
+
+		MainAdapter adapter = new MainAdapter();
+		rvMain.setAdapter(adapter);
+	}
+	private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+
+		@NonNull
+		@Override
+		public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false ));
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+
+		}
+
+		@Override
+		public int getItemCount() {
+			return 15;
+		}
+	}
+
+	private class MainViewHolder extends RecyclerView.ViewHolder{
+
+		public MainViewHolder(@NonNull View itemView) {
+			super(itemView);
+		}
 	}
 }
